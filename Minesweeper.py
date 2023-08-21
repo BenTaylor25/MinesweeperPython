65d751ddcb14af924ea0f848b15201851e5e7637
benlaptop25 <ben.taylor2573@gmail.com>
2023-08-21 06:31:28 +0100

    added space after arrow prompt

--- Minesweeper.py
+++ Minesweeper.py
@@ -20,7 +20,7 @@ def menu_handler(options: Dict[str, Callable[[], None]]) -> str:
     choice = ""
     option_numbers = [str(i+1) for i in range(len(options))]   # ["1", "2", ...]
     while choice not in option_numbers:
-        choice = input("->")
+        choice = input("-> ")
 
     # call function of selected option
     options[list(options)[int(choice)-1]]()
