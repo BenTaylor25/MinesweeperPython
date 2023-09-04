f1091acef0ce9d2b91dbb16ecbadb1c9d142883a
Ben Taylor <ben.taylor2573@gmail.com>
2023-09-04 19:41:13 +0100

    added width setting

--- settings.py
+++ settings.py
@@ -6,10 +6,27 @@ MINE_COUNT = 30
 
 def settings_menu():
     while menu_handler("Settings", {
-        "Set Width": set_width,
+        f"Set Width ({WIDTH})": set_width,
         "Back": (lambda : None)
     }) != "Back":
         pass
 
 def set_width():
-    pass
+    new_width_str = input("New Width: ")
+
+    if not new_width_str.isnumeric():
+        print("Failed: width must be a positive whole number.")
+        return
+
+    new_width = int(new_width_str)
+
+    if new_width < 1:
+        print("Failed: width is too small.")
+        return
+
+    if new_width > 20:
+        print("Failed: width is too large.")
+        return
+
+    global WIDTH
+    WIDTH = new_width
