b6c0f4b1d708bc8f451ac1ae63497b90adc759df
benlaptop25 <ben.taylor2573@gmail.com>
2023-09-05 06:56:37 +0100

    added settings for height and mine count

--- settings.py
+++ settings.py
@@ -7,17 +7,40 @@ MINE_COUNT = 30
 def settings_menu():
     while menu_handler("Settings", {
         f"Set Width ({WIDTH})": set_width,
+        f"Set Height ({HEIGHT})": set_height,
+        f"Set Mine Count ({MINE_COUNT})": set_mine_count,
         "Back": (lambda : None)
     }) != "Back":
         pass
 
+
 def set_width():
-    new_width = get_int_in_range("width", 20)
+    new_width = get_setting_as_int_in_range("width", 20)
+
     if new_width != -1:
         global WIDTH
         WIDTH = new_width
 
-def get_int_in_range(setting_name: str, max_value: int) -> int:
+def set_height():
+    new_height = get_setting_as_int_in_range("height", 20)
+
+    if new_height != -1:
+        global HEIGHT
+        HEIGHT = new_height
+
+def set_mine_count():
+    new_mine_count = get_setting_as_int_in_range("mine count", 300)
+
+    if new_mine_count > WIDTH * HEIGHT:
+        print("Failed: Mine Count cannot be greater than the number of tiles on the grid.")
+        return
+
+    if new_mine_count != -1:
+        global MINE_COUNT
+        MINE_COUNT = new_mine_count
+
+
+def get_setting_as_int_in_range(setting_name: str, max_value: int) -> int:
     new_value_str = input(f"New {setting_name.capitalize()}: ")
 
     if not new_value_str.isnumeric():
@@ -30,7 +53,7 @@ def get_int_in_range(setting_name: str, max_value: int) -> int:
         print(f"Failed: {setting_name} is too small.")
         return -1
 
-    if new_value > 20:
+    if new_value > max_value:
         print(f"Failed: {setting_name} is too large.")
         return -1
     
