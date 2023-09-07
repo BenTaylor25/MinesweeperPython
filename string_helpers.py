da036eaada2b633de3f9c85af88adb867467ef31
Ben Taylor <ben.taylor2573@gmail.com>
2023-09-07 19:08:15 +0100

    fixed capitalise_words() function

--- string_helpers.py
+++ string_helpers.py
@@ -4,4 +4,4 @@ def capitalise_words(words: str) -> str:
     Capitalise the first letter of each word.
     E.g. "hello world" -> "Hello World"
     """
-    return [word.capitalize() for word in words.split()]
+    return " ".join([word.capitalize() for word in words.split()])
